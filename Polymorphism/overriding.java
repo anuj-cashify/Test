6ad5349ccf7a618ec6ab91d6167ba15134711f3e
Anuj Gupta <anuj.g@cashify.in>
2021-05-10 18:29:03 +0530

    test

--- Polymorphism/overriding.java
+++ Polymorphism/overriding.java
@@ -15,7 +15,7 @@ class overriding2 extends overriding {
 
     public static void main(String[] args) {
         overriding t = new overriding();
-        t.show();
+       // t.show();
 
         overriding2 q = new overriding2();
         q.show();
